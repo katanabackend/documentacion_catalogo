bd67cc7e261f35960e7a690d2e209a68c41586f7
seguimiento-melonn <49932526+seguimiento-melonn@users.noreply.github.com>
2020-12-11 17:52:21 -0600

    |

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout :sidebar="false">
     <div class="content">
-      <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
+      <h1>{{ $static.metadata.siteName }} | {{ this.description }}</h1>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
         <Shortcut link="/mision" text="Misión" icon="target-icon" />
